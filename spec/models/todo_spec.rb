320fb7feadb927a3f50507732a2363056bb3115f
Anthony Scinocco <scinocco.a@gmail.com>
2018-03-28 17:06:40 -0400

    wrote Rspec test for todo model

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
+# test suite for todo model
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # test if todo model has 1 to many relation ship with item modal
+  it { should have_many(:items).dependant(:destroy) }
+
+  # Validation tests
+  # check the todo has a title
+  it { should validate_presence_of(:title) }
+  # check that todo has a created_by user
+  it { should validate_presence_of(:created_by) }
 end
