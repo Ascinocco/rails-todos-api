4e8b6b567386fe3397dd47e5a0192a4de9d12fb3
Anthony Scinocco <scinocco.a@gmail.com>
2018-03-28 17:08:48 -0400

    added test for item model

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
+# test suite for item model
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # association test
+  # esure an item belongs to a single todo record
+  it { should belong_to(:todo) }
+
+  # ensure an item has a name
+  it { should validate_presence_of(:name) }
 end
